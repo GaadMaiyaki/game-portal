0f8d27d68d74dd2161f1bba882d92305e0f02486
GaadMaiyaki <petershola18@gmail.com>
2025-03-06 17:35:41 +0100

    chore: setup lint rules

--- packages/app/eslint.config.mjs
+++ packages/app/eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,54 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:@typescript-eslint/recommended'
+  ),
+  ...compat.plugins('@typescript-eslint', 'prettier'),
+  ...compat.config(
+    { parser: '@typescript-eslint/parser' },
+    {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: ['./tsconfig.json'],
+        createDefaultProgram: true,
+      },
+    }
+  ),
+  {
+    settings: {
+      workingDirectories: [
+        {
+          mode: 'auto',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      'no-console': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/extensions': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-param-reassign': [
+        'error',
+        {
+          props: true,
+          ignorePropertyModificationsFor: ['state'],
+        },
+      ],
+      'import/prefer-default-export': 'off',
+      'no-nested-ternary': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
