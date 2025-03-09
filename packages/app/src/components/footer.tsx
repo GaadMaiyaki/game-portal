696428caea1b24f92cbb23db52882a2a06cea540
GaadMaiyaki <petershola18@gmail.com>
2025-03-09 21:23:10 +0100

    feat(footer): enhance Footer component to display dynamic brand data

--- packages/app/src/components/footer.tsx
+++ packages/app/src/components/footer.tsx
@@ -1,15 +1,21 @@
-const Footer = () => {
+import { BrandConfigProps } from '@/lib/configs/brand';
+
+type FooterProps = {
+  brandConfigData: BrandConfigProps;
+};
+
+const Footer = ({ brandConfigData }: FooterProps) => {
   return (
-    <footer className="w-full bg-gray-900 dark:bg-gray-900 py-6 shadow-2xl drop-shadow-lg dark:border-gray-700">
+    <footer className="w-full bg-gameportal-footer-bg py-6 shadow-2xl drop-shadow-lg dark:border-gray-700">
       <div className="container mx-auto px-6 lg:px-16 flex flex-col md:flex-row items-center justify-between">
         <div className="flex items-center gap-3">
-          <span className="text-lg font-semibold text-gray-100 dark:text-gray-200">
-            Game Portal
+          <span className="text-lg font-semibold text-gameportal-footer-text dark:text-gray-200">
+            {brandConfigData.brandName}
           </span>
         </div>
 
-        <p className="text-xs text-gray-300 dark:text-gray-400 mt-4 md:mt-0">
-          &copy; {new Date().getFullYear()} Game Portal. All rights reserved.
+        <p className="text-xs text-gameportal-footer-text mt-4 md:mt-0">
+          {brandConfigData.footer.text}
         </p>
       </div>
     </footer>
