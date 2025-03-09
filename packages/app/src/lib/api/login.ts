1af4715e569a1455e9f1a540ed27a9cdebe6888b
GaadMaiyaki <petershola18@gmail.com>
2025-03-09 16:29:06 +0100

    refactor: use global UserDataWithoutPassword type in login function

--- packages/app/src/lib/api/login.ts
+++ packages/app/src/lib/api/login.ts
@@ -1,11 +1,9 @@
 import { API_ENDPOINTS } from '@game-portal/constants';
-import { UserData } from '@game-portal/types';
+import { UserDataWithoutPassword } from '@game-portal/types';
 
 import HTTPService from '../http-service';
 import { LoginDataProps } from '../schemas/login';
 
-type LoginResponse = Omit<UserData, 'password'>;
-
 export const loginUser = async (data: LoginDataProps) => {
-  return await HTTPService.post<LoginResponse>(API_ENDPOINTS.LOGIN, data);
+  return await HTTPService.post<UserDataWithoutPassword>(API_ENDPOINTS.LOGIN, data);
 };
