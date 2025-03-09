fd516a449790ccd1c2c5f6dbd5745fe6f1479496
GaadMaiyaki <petershola18@gmail.com>
2025-03-09 21:22:07 +0100

    feat(layout): integrate BrandConfigProvider into MarketLayout for brand data

--- packages/app/src/app/[market]/layout.tsx
+++ packages/app/src/app/[market]/layout.tsx
@@ -5,19 +5,25 @@ import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import AuthProvider from '@/lib/auth/auth-provider';
 import { getSession } from '@/lib/helpers/session';
+import BrandConfigProvider from '@/lib/brand/brand-provider';
+import { getBrandConfigFromEnv } from '@/lib/helpers/getBrandConfigFromEnv';
 
 const MarketLayout = async ({ children }: { children: React.ReactNode }) => {
   const userData = await getSession<UserDataWithoutPassword>(
     SESSION_KEYS.USER_SESSION
   );
 
+  const brandConfigData = getBrandConfigFromEnv();
+
   return (
     <AuthProvider userData={userData}>
-      <section className="w-full h-full p-0 m-0">
-        <Navbar />
-        {children}
-        <Footer />
-      </section>
+      <BrandConfigProvider brandConfigData={brandConfigData}>
+        <section className="w-full h-full p-0 m-0">
+          <Navbar brandConfigData={brandConfigData} />
+          {children}
+          <Footer brandConfigData={brandConfigData} />
+        </section>
+      </BrandConfigProvider>
     </AuthProvider>
   );
 };
