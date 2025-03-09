005b712354d66ac5b83a1f954c07f34574a0a897
GaadMaiyaki <petershola18@gmail.com>
2025-03-09 14:07:10 +0100

    chore(config): allow images from external domains

--- packages/app/next.config.ts
+++ packages/app/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["flagcdn.com", "casinodays2.imgix.net"],
+  },
 };
 
 export default nextConfig;
