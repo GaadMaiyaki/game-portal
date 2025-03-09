d1643f3cd0eb7ba252c300f6ba92b7df243b1da8
GaadMaiyaki <petershola18@gmail.com>
2025-03-09 13:53:01 +0100

    feat(middleware): add market-based routing and user session validation

---
+++ packages/app/src/middleware.ts
@@ -0,0 +1,43 @@
+import { SESSION_KEYS } from '@game-portal/constants';
+import { UserDataWithoutPassword } from '@game-portal/types';
+import { NextRequest, NextResponse } from 'next/server';
+import { MARKETS } from '@game-portal/constants';
+
+const LOGIN_ROUTE = 'login';
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  const marketParam = url.pathname.split('/')[1];
+
+  const availableMarkets = Object.values(MARKETS);
+
+  const isAccessingMarketRoute = availableMarkets
+    .map((availableMarket) => availableMarket.toLocaleLowerCase())
+    .includes(marketParam.toLocaleLowerCase());
+
+  const session = req.cookies.get(SESSION_KEYS.USER_SESSION);
+
+  const userData = (session ? JSON.parse(session?.value || '{}') : session) as
+    | UserDataWithoutPassword
+    | undefined;
+
+  const userMarket = userData?.registrationCountry;
+
+  if (!userData && marketParam !== LOGIN_ROUTE) {
+    return NextResponse.redirect(new URL(`/${LOGIN_ROUTE}`, req.url));
+  }
+
+  if (userData && userMarket !== marketParam && isAccessingMarketRoute) {
+    const pathnameToRedirect = `/${userData?.registrationCountry}${url.pathname.substring(3)}`;
+
+    return NextResponse.redirect(new URL(pathnameToRedirect, req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
