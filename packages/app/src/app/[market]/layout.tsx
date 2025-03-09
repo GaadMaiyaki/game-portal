f9c0a515a0c94ebdc87c814f5c4b9a14ab5fc63e
GaadMaiyaki <petershola18@gmail.com>
2025-03-09 13:43:31 +0100

    feat: create MarketLayout component with authentication context

---
+++ packages/app/src/app/[market]/layout.tsx
@@ -0,0 +1,25 @@
+import { SESSION_KEYS } from '@game-portal/constants/dist';
+import { UserDataWithoutPassword } from '@game-portal/types';
+
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
+import AuthProvider from '@/lib/auth/auth-provider';
+import { getSession } from '@/lib/helpers/ssesion';
+
+const MarketLayout = async ({ children }: { children: React.ReactNode }) => {
+  const userData = await getSession<UserDataWithoutPassword>(
+    SESSION_KEYS.USER_SESSION
+  );
+
+  return (
+    <AuthProvider userData={userData}>
+      <section className="w-full h-full p-0 m-0">
+        <Navbar />
+        {children}
+        <Footer />
+      </section>
+    </AuthProvider>
+  );
+};
+
+export default MarketLayout;
