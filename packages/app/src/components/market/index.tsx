448cf5f4f9a5595a8b2a7a966691e76f71d7b85d
GaadMaiyaki <petershola18@gmail.com>
2025-03-10 11:29:21 +0100

    refactor: replace getMarketConfig with store selector for brand configuration data

--- packages/app/src/components/market/index.tsx
+++ packages/app/src/components/market/index.tsx
@@ -1,29 +1,33 @@
 'use client';
 
-import { getMarketConfig } from '@/lib/utils/getMarketConfig';
 import { useGetUserData } from '@/hooks/useGetUserData';
+import { useAppSelector } from '@/lib/store';
+import { selectBrandConfigData } from '@/lib/store/features/brand-config/brand-config-slice';
 
 import MarketInfo from './market-info';
 import FeatureList from './feature-list';
-import AvailableBrands from './available-brands';
 
 const Welcome = () => {
   const userData = useGetUserData();
 
-  const marketConfigData = getMarketConfig(userData?.registrationCountry);
+  const brandConfigData = useAppSelector(selectBrandConfigData);
+
+  if (!userData) return undefined;
+
+  const marketConfigData =
+    brandConfigData?.markets[userData?.registrationCountry];
 
   if (!marketConfigData) return undefined;
 
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center px-6 bg-gray-900 text-white">
-      <div className="max-w-4xl w-full bg-gray-800 p-8 rounded-lg shadow-lg">
+    <section className="h-[82vh] flex flex-col justify-center items-center px-6 bg-gameportal-background text-white">
+      <div className="max-w-4xl w-full bg-gameportal-background border border-gameportal-border p-8 rounded-lg shadow-lg">
         <MarketInfo
           title={marketConfigData?.title}
           description={marketConfigData?.description}
           flag={marketConfigData?.flag}
         />
         <FeatureList features={marketConfigData?.features} />
-        <AvailableBrands brands={marketConfigData?.availableBrands} />
       </div>
     </section>
   );
