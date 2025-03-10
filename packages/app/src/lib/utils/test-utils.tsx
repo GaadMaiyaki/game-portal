044ebe67628c56be36aee160ecda6dc1fb8cfbfe
GaadMaiyaki <petershola18@gmail.com>
2025-03-10 21:35:47 +0100

    refactor: replace custom StoreProvider with Redux Provider and support preloadedState in test utilities

--- packages/app/src/lib/utils/test-utils.tsx
+++ packages/app/src/lib/utils/test-utils.tsx
@@ -1,37 +1,29 @@
 import { render, renderHook, RenderOptions } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { UserDataWithoutPassword } from '@game-portal/types';
+import { Provider } from 'react-redux';
 
-import { StoreProvider } from '@/lib/store';
+import { AppStore, makeStore, RootState } from '@/lib/store';
 import ReactQueryProvider from '@/lib/react-query-provider';
-import AuthProvider from '@/lib/auth/auth-provider';
-import BrandConfigProvider from '@/lib/brand/brand-provider';
 import { Toaster } from '@/components/ui/sonner';
-
-import { BRAND_CONFIG } from '../configs/brand';
 
 type ExtendedRenderOptions = Omit<RenderOptions, 'queries'> & {
   userData?: UserDataWithoutPassword;
+  preloadedState?: Partial<RootState>;
+  store?: AppStore;
 };
 
 function TestProviders({
   children,
-  userData,
+  store,
 }: {
   children: ReactNode;
   userData?: UserDataWithoutPassword;
+  store: AppStore;
 }) {
-  const brandConfigData = BRAND_CONFIG['CasinoA'];
-
   return (
     <ReactQueryProvider>
-      <StoreProvider>
-        <AuthProvider userData={userData}>
-          <BrandConfigProvider brandConfigData={brandConfigData}>
-            {children}
-          </BrandConfigProvider>
-        </AuthProvider>
-      </StoreProvider>
+      <Provider store={store}>{children}</Provider>
       <Toaster />
     </ReactQueryProvider>
   );
@@ -39,21 +31,33 @@ function TestProviders({
 
 export function renderWithProviders(
   ui: React.ReactElement,
-  options?: ExtendedRenderOptions
+  {
+    preloadedState,
+    store = makeStore(preloadedState),
+    ...options
+  }: ExtendedRenderOptions = {}
 ) {
   return render(
-    <TestProviders userData={options?.userData}>{ui}</TestProviders>,
+    <TestProviders userData={options?.userData} store={store}>
+      {ui}
+    </TestProviders>,
     options
   );
 }
 
 export function renderHookWithProviders<Result, Props>(
   hookRenderCallback: (initialProps: Props) => Result,
-  options?: ExtendedRenderOptions
+  {
+    preloadedState,
+    store = makeStore(preloadedState),
+    ...options
+  }: ExtendedRenderOptions = {}
 ) {
   return renderHook(hookRenderCallback, {
     wrapper: ({ children }) => (
-      <TestProviders userData={options?.userData}>{children}</TestProviders>
+      <TestProviders userData={options?.userData} store={store}>
+        {children}
+      </TestProviders>
     ),
     ...options,
   });
