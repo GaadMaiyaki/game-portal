be1210d033cb8c52e9756caffd0ada59215d6f39
GaadMaiyaki <petershola18@gmail.com>
2025-03-10 11:18:17 +0100

    feat: add gridStyleData for dynamic grid layouts for styling CasinoGameGrid

---
+++ packages/app/src/lib/configs/grid.ts
@@ -0,0 +1,16 @@
+export type GridValues = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+export const gridStyleData: Record<GridValues, string> = {
+  1: 'lg:col-span-1 xl:col-span-1 2xl:col-span-1',
+  2: 'lg:col-span-2 xl:col-span-2 2xl:col-span-2',
+  3: 'lg:col-span-3 xl:col-span-3 2xl:col-span-3',
+  4: 'lg:col-span-4 xl:col-span-4 2xl:col-span-4',
+  5: 'lg:col-span-5 xl:col-span-5 2xl:col-span-5',
+  6: 'lg:col-span-6 xl:col-span-6 2xl:col-span-6',
+  7: 'lg:col-span-7 xl:col-span-7 2xl:col-span-7',
+  8: 'lg:col-span-8 xl:col-span-8 2xl:col-span-8',
+  9: 'lg:col-span-9 xl:col-span-9 2xl:col-span-9',
+  10: 'lg:col-span-10 xl:col-span-10 2xl:col-span-10',
+  11: 'lg:col-span-11 xl:col-span-11 2xl:col-span-11',
+  12: 'lg:col-span-12 xl:col-span-12 2xl:col-span-12',
+};
