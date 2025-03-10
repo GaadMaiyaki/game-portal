6ed23d19d897760a806f47e01e449a2e145a7405
GaadMaiyaki <petershola18@gmail.com>
2025-03-10 11:04:59 +0100

    feat: enhance CasinoGameCard with dynamic grid column styling based on brand configuration

--- packages/app/src/components/casino/casino-card.tsx
+++ packages/app/src/components/casino/casino-card.tsx
@@ -2,15 +2,30 @@ import { MappedGameProps, Market } from '@game-portal/types';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { cn } from '@/lib/utils';
+import { gridStyleData, GridValues } from '@/lib/configs/grid';
+
 const CasinoGameCard = ({
   id,
   name,
   thumbnail,
   userMarket,
   slug,
-}: MappedGameProps & { userMarket: Market | undefined }) => {
+  gridColumn,
+}: MappedGameProps & {
+  userMarket: Market | undefined;
+  gridColumn: number;
+}) => {
+  const gridStyle = gridStyleData[gridColumn as GridValues];
   return (
-    <div key={id} className="p-2 rounded-md shadow  cursor-pointer">
+    <div
+      key={id}
+      className={cn(
+        'p-2 rounded-md shadow  cursor-pointer',
+        'col-span-12 sm:col-span-6 md:col-span-4',
+        gridStyle
+      )}
+    >
       <Link href={`/${userMarket}/casino/${slug}`}>
         <Image
           height={100}
