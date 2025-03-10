45e8ac665eb0755f77f181c6a0e217ef336235e4
GaadMaiyaki <petershola18@gmail.com>
2025-03-10 11:34:33 +0100

    feat: pass userData prop to ProfileDetail and apply brand-specific styling

--- packages/app/src/components/user-profile/profile-detail.tsx
+++ packages/app/src/components/user-profile/profile-detail.tsx
@@ -1,48 +1,69 @@
 'use client';
 
+import { UserDataWithoutPassword } from '@game-portal/types';
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { useAppSelector } from '@/lib/store';
-import { selectUserData } from '@/lib/store/features/auth/auth-slice';
 
 type ProfileDetailProps = {
   handleToggleEditState: (status: boolean) => void;
+  userData: UserDataWithoutPassword;
 };
 
-const ProfileDetail = ({ handleToggleEditState }: ProfileDetailProps) => {
-  const user = useAppSelector(selectUserData);
-
-  if (!user) return undefined;
-
+const ProfileDetail = ({
+  handleToggleEditState,
+  userData,
+}: ProfileDetailProps) => {
   return (
-    <div className="flex justify-center items-center min-h-screen p-4">
-      <Card className="w-full max-w-lg p-6 shadow-lg">
+    <div className="flex justify-center items-center h-[82vh] p-4">
+      <Card className="w-full max-w-lg p-6 shadow-lg bg-gameportal-card-bg border-none">
         <CardHeader>
-          <CardTitle className="text-center text-xl font-semibold">
+          <CardTitle className="text-center text-gameportal-secondary-text text-xl font-semibold">
             Profile
           </CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">
           <div className="flex flex-col space-y-2">
-            <p className="text-gray-700 font-medium">
-              Username: <span className="font-normal">{user.username}</span>
+            <p className="font-medium">
+              <span className="text-gameportal-secondary-text">Username:</span>
+              &nbsp;
+              <span className="text-gameportal-primary-text font-normal">
+                {userData.username}
+              </span>
             </p>
-            <p className="text-gray-700 font-medium">
-              Email: <span className="font-normal">{user.email}</span>
+            <p className=" font-medium">
+              <span className="text-gameportal-secondary-text">Email:</span>
+              &nbsp;
+              <span className="text-gameportal-primary-text font-normal">
+                {userData.email}
+              </span>
             </p>
-            <p className="text-gray-700 font-medium">
-              First Name: <span className="font-normal">{user.firstName}</span>
+            <p className="font-medium">
+              <span className="text-gameportal-secondary-text">
+                First Name:
+              </span>
+              &nbsp;
+              <span className="text-gameportal-primary-text font-normal">
+                {userData.firstName}
+              </span>
             </p>
-            <p className="text-gray-700 font-medium">
-              Last Name: <span className="font-normal">{user.lastName}</span>
+            <p className="text-gameportal-primary-text font-medium">
+              <span className="text-gameportal-secondary-text">Last Name:</span>
+              &nbsp;
+              <span className="text-gameportal-primary-text font-normal">
+                {userData.lastName}
+              </span>
             </p>
-            <p className="text-gray-700 font-medium">
-              Country:{' '}
-              <span className="font-normal">{user.registrationCountry}</span>
+            <p className="font-medium">
+              <span className="text-gameportal-secondary-text">Country:</span>
+              &nbsp;
+              <span className="text-gameportal-primary-text font-normal">
+                {userData.registrationCountry}
+              </span>
             </p>
           </div>
           <Button
-            className="w-full py-6 cursor-pointer"
+            className="w-full py-6 mt-4 cursor-pointer bg-gameportal-button-bg text-gameportal-button-text hover:bg-gameportal-button-bg hover:opacity-80"
             onClick={() => handleToggleEditState(true)}
           >
             Edit Profile
